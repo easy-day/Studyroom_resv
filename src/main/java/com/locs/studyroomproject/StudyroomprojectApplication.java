31639743c7243b707bb86fa68d3e63c7c6e4ab0c
jihun <wlgns4307@gmail.com>
2019-11-06 18:56:03 +0900

    Initial commit

---
+++ src/main/java/com/locs/studyroomproject/StudyroomprojectApplication.java
@@ -0,0 +1,13 @@
+package com.locs.studyroomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyroomprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyroomprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/locs/studyroomproject/StudyroomprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locs.studyroomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyroomprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
